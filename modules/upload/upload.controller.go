eaa6d69ed9289562094196c7d56926bc86ceb0da
romzifg <romzifg@gmail.com>
2024-01-07 14:15:16 +0700

    update, add function for multiple upload file

--- modules/upload/upload.controller.go
+++ modules/upload/upload.controller.go
@@ -2,6 +2,7 @@ package upload
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,15 +11,59 @@ func UploadFile(c *fiber.Ctx) error {
 	file, err := c.FormFile("file")
 
 	if err != nil {
-		return err
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"statusCode": http.StatusBadRequest,
+			"message": err.Error(),
+			"data": nil,
+		})
 	}
 
 	destination := fmt.Sprintf("./public/files/%s", file.Filename)
 	if err = c.SaveFile(file, destination); err != nil {
-		return err
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"statusCode": http.StatusBadRequest,
+			"message": err.Error(),
+			"data": nil,
+		})
 	}
 
-	return c.Status(200).JSON(fiber.Map{
+	return c.Status(http.StatusOK).JSON(fiber.Map{
+		"statusCode": http.StatusOK,
+		"message": "Success",
 		"data": destination,
+	})
+}
+
+func MultipleUpload(c *fiber.Ctx) error {
+	form, err := c.MultipartForm()
+	var listDest []string;
+
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"statusCode": http.StatusBadRequest,
+			"message": err.Error(),
+			"data": nil,
+		})
+	}
+
+	for _, fileHeaders := range form.File {
+		for _, fileHeader := range fileHeaders {
+			destination := fmt.Sprintf("./public/files/%s", fileHeader.Filename)
+			if err = c.SaveFile(fileHeader, destination); err != nil {
+					return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+					"statusCode": http.StatusBadRequest,
+					"message": err.Error(),
+					"data": nil,
+				})
+			}
+
+			listDest = append(listDest, destination)
+		}
+	}
+
+	return c.Status(http.StatusOK).JSON(fiber.Map{
+		"statusCode": http.StatusOK,
+		"message": "Success",
+		"data": listDest,
 	})
 }
--- modules/upload/upload.routes.go
+++ modules/upload/upload.routes.go
@@ -5,5 +5,6 @@ import "github.com/gofiber/fiber/v2"
 func Routes(app *fiber.App) {
 	api := app.Group("/api/v1/upload")
 
-	api.Post("/", UploadFile)
+	api.Post("/file", UploadFile)
+	api.Post("/files", MultipleUpload)
 }
