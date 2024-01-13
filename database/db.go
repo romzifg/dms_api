16edc331094b0874a5e56757d4408328fc658b07
romzifg <romzifg@gmail.com>
2024-01-13 23:36:43 +0700

    update, update postgresql connection

--- database/db.go
+++ database/db.go
@@ -7,6 +7,7 @@ import (
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 var DB *gorm.DB
@@ -18,7 +19,10 @@ func DbConnect() {
 	}
  
 	dsn := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
+		TablePrefix: "dms.",
+        SingularTable: false,
+	}})
 
 	if err != nil {
 		log.Fatal("Error connect to databse")
